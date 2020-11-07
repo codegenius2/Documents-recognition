f06ad6bc4ac15b563a71ca3cae616ae690a19493
Hleb Albau <hleb.albau@gmail.com>
2020-11-07 20:07:06 +0300

    advanced text field extraction

--- src/ext/text.ts
+++ src/ext/text.ts
@@ -1,50 +1,66 @@
 import {
-    TextAvailableSource as AvailableSource,
-    CheckResult,
-    Text as BaseText,
-    TextFieldType
+  TextAvailableSource as AvailableSource,
+  CheckResult,
+  Text as BaseText,
+  TextFieldType, LCID
 } from "../models/index.js";
 import {TextField} from "./text-field.js";
 
 
 export class Text implements BaseText {
 
-    status: CheckResult;
-    validityStatus: CheckResult;
-    comparisonStatus: CheckResult;
+  status: CheckResult;
+  validityStatus: CheckResult;
+  comparisonStatus: CheckResult;
 
-    availableSourceList: Array<AvailableSource>;
-    fieldList: Array<TextField>;
+  availableSourceList: Array<AvailableSource>;
+  fieldList: Array<TextField>;
 
-    constructor(origin: BaseText) {
-        this.status = origin.status
-        this.validityStatus = origin.validityStatus
-        this.comparisonStatus = origin.comparisonStatus
-        this.availableSourceList = origin.availableSourceList
-        this.fieldList = origin.fieldList.map(field => new TextField(field))
-    }
+  constructor(origin: BaseText) {
+    this.status = origin.status
+    this.validityStatus = origin.validityStatus
+    this.comparisonStatus = origin.comparisonStatus
+    this.availableSourceList = origin.availableSourceList
+    this.fieldList = origin.fieldList.map(field => new TextField(field))
+  }
 
-    public getField(type: TextFieldType, lcid?: number): TextField | undefined {
-        return this.fieldList.find(field => field.fieldType == type && (!lcid || field.lcid == lcid))
-    }
-
-    public getFieldValue(type: TextFieldType, lcid?: number): string | undefined {
-        const field = this.getField(type, lcid)
-        if (field) {
-            return field.getValue()
+  public getField(type: TextFieldType, lcid?: number): TextField | undefined {
+    let result = undefined
+    for (const field of this.fieldList) {
+      if (field.fieldType == type) {
+        if (lcid != undefined && field.lcid == lcid) {
+          return field
+        } else if (lcid == undefined && field.lcid == LCID.LATIN) {
+          return field
+        } else if (lcid == undefined && result == undefined) {
+          result = field
         }
-        return undefined
+      }
     }
+    return result
+  }
 
-    public getFieldByName(name: string, lcid?: number): TextField | undefined {
-        return this.fieldList.find(field => field.fieldName == name && (!lcid || field.lcid == lcid))
-    }
-
-    public getFieldValueByName(name: string, lcid?: number): string | undefined {
-        const field = this.getFieldByName(name, lcid)
-        if (field) {
-            return field.getValue()
+  public getFieldByName(name: string, lcid?: number): TextField | undefined {
+    let result = undefined
+    for (const field of this.fieldList) {
+      if (field.fieldName == name) {
+        if (lcid != undefined && field.lcid == lcid) {
+          return field
+        } else if (lcid == undefined && field.lcid == LCID.LATIN) {
+          return field
+        } else if (lcid == undefined && result == undefined) {
+          result = field
         }
-        return undefined
+      }
     }
+    return result
+  }
+
+  public getFieldValue(type: TextFieldType, lcid?: number): string | undefined {
+    return this.getField(type, lcid)?.getValue()
+  }
+
+  public getFieldValueByName(name: string, lcid?: number): string | undefined {
+    return this.getFieldByName(name, lcid)?.getValue()
+  }
 }
